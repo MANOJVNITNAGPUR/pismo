426d01260fea00b4e7fb0be6dfe3bd51ee38ab69
Manoj Rewatkar <manoj.rewatkar@paysafe.com>
2022-06-24 21:46:34 +0530

    Fixed issue with fetch txn repo method

--- src/main/java/io/pismo/repo/TransactionRepository.java
+++ src/main/java/io/pismo/repo/TransactionRepository.java
@@ -3,6 +3,7 @@ package io.pismo.repo;
 import io.pismo.dao.TransactionDao;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +13,7 @@ import java.util.Optional;
 public interface TransactionRepository extends PagingAndSortingRepository<TransactionDao,Long> {
     Optional<TransactionDao> findByTransactionId(Long transactionId);
 
-    @Query("select * from transactions where balanceAmount<0.0 and accountId=:accountId")
-    List<TransactionDao> getAllBalanceTransactions(long accountId);
+    @Query("select txn from transactions txn where txn.balanceAmount < 0.0 and txn.accountId = :accountId")
+    List<TransactionDao> getAllBalanceTransactions(@Param("accountId") long accountId);
 
 }
